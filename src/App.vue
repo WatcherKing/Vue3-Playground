0eb52a4f0733ff3ecf11720b80ac67164defd8ab
daking24 <daking3968@outlook.com>
2022-12-04 23:40:15 +0100

    inits

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  data() {
+    return {
+      msg: 'Hello World'
+    }
+  }
+})
+</script>
