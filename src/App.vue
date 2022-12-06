67b684b2cc076a4ee9dd834736c029f6b37fff3a
daking24 <daking3968@outlook.com>
2022-12-06 06:14:59 +0100

    Methods, Computed Properties

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,72 @@
 <template>
-  <h1>Marvel Heroes</h1>
-  <ol>
+  <h1  >{{ title }} {{ herosCount }}</h1>
+  <h2>{{ fullName }}</h2>
+  <p>{{ randC }}</p>
+  <p>{{ randC }}</p>
+  <p>{{ randC }}</p>
+  <p>{{ randM() }}</p>
+  <p>{{ randM() }}</p>
+  <p>{{ randM() }}</p>
+
+  <ul>
     <li v-for="(hero, index) in marvelHeroes" :key="index">{{ hero.name }}</li>
-  </ol>
+  </ul>
+  <form @submit.prevent="addHero">
+    <input type="text" v-model.lazy="newHero" placeholder="Type Hero name here">
+    <button type="submit">Add hero</button>
+  </form>
+  <button @click="setFullName">Change Name</button>
+  <small>Total heros are {{ herosCount }}</small>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'App',
   data() {
     return {
+      title: "Marvel Hero",
+      newHero: "",
+      fname: "John",
+      lname: "Doe",
       marvelHeroes: [{name: 'Thor'},
                      {name: 'Iron-Man'},
                      {name: 'Hulk'}, 
                      {name: 'Captain America'}],
+    }
+  },
+  methods: {
+    addHero() {
+      if (this.newHero !== "") {
+        this.marvelHeroes.push({name: this.newHero})
+        this.newHero = ""
+      }
+    },
+    randM(): number {
+      return Math.random()
+    },
+    setFullName() {
+      this.fullName = 'King David'
+    }
+  },
+  computed: {
+    herosCount(): number {
+      return this.marvelHeroes.length
+    },
+    randC(): number {
+      return this.herosCount + Math.random()
+    },
+    fullName: {
+      get(): string {
+        return this.fname + " " + this.lname
+      },
+      set(fullName: string) {
+        const names = fullName.split(" ")
+        this.fname = names[0]
+        this.lname = names[1]
+
+      }
     }
   }
 })
