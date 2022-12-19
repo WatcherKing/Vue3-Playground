671837702740b6526ee8c04946c70ec6e8e94c23
daking24 <daking3968@outlook.com>
2022-12-19 23:17:34 +0100

    Composition API

--- src/pages/Calculator.vue
+++ src/pages/Calculator.vue
@@ -3,11 +3,16 @@
 		<div class="m-auto">
 			<h1 class="text-2xl text-center">Calculator</h1>
 			<p
-				class="mt-10 mb-2 text-3xl w-32 text-right border rounded overflow-x-scroll"
+				class="mt-10 text-3xl w-42 h-20 text-right border rounded overflow-x-scroll"
 				style="direction: rtl"
 			>
-				{{ calculations }}
+				{{ currentNum }}
 			</p>
+			<div class="h-10">
+				<small v-if="selectedOp">
+					{{ prevNum }} {{ selectedOp }} {{ currentNum }}
+				</small>
+			</div>
 			<div class="grid grid-cols-4 gap-1">
 				<button
 					@click="pressed('1')"
@@ -82,8 +87,14 @@
 					*
 				</button>
 				<button
+					@click="pressed('c')"
+					class="p-2 w-10 h-10 border rounded shadow border-grey"
+				>
+					C
+				</button>
+				<button
 					@click="pressed('0')"
-					class="p-2 h-10 border rounded shadow border-grey col-span-2"
+					class="p-2 w-10 h-10 border rounded shadow border-grey"
 				>
 					0
 				</button>
@@ -105,31 +116,90 @@
 </template>
 
 <script lang="ts">
-import { calculateBackoffMillis } from "@firebase/util";
-import { defineComponent, ref } from "vue";
+import { defineComponent, onMounted, onUnmounted, ref } from "vue";
 
 export default defineComponent({
 	setup() {
-		const calculations = ref("");
+		// const calculations = ref("");
 		const ops = ["+", "-", "*", "/"];
+		const currentNum = ref("");
+		const prevNum = ref("");
+		const selectedOp = ref("");
+		const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 		function pressed(value: string) {
-			if (value === "=") calculate();
+			if (value === "=" || value === "Enter") calculate();
+			else if (value == "c") clear();
 			else if (ops.includes(value)) applyOperation(value);
-			else appendNumber(value);
+			else if (numbers.includes(value)) appendNumber(value);
 		}
 
 		function appendNumber(value: string) {
-			calculations.value = calculations.value + value;
+			currentNum.value = currentNum.value + value;
 		}
 
-		function applyOperation(value: string) {}
+		function applyOperation(value: string) {
+			calculate();
+			prevNum.value = currentNum.value;
+			currentNum.value = "";
+			selectedOp.value = value;
+		}
 
-		function calculate() {}
+		function calculate() {
+			if (selectedOp.value === "*") multiply();
+			else if (selectedOp.value === "/") divide();
+			else if (selectedOp.value === "-") minus();
+			else if (selectedOp.value === "+") sum();
 
+			prevNum.value = "";
+			selectedOp.value = "";
+		}
+
+		function multiply() {
+			const value = (
+				parseInt(prevNum.value) * parseInt(currentNum.value)
+			).toString();
+			currentNum.value = value;
+		}
+
+		function divide() {
+			const value = (
+				parseInt(prevNum.value) / parseInt(currentNum.value)
+			).toString();
+			currentNum.value = value;
+		}
+
+		function sum() {
+			const value = (
+				parseInt(prevNum.value) + parseInt(currentNum.value)
+			).toString();
+			currentNum.value = value;
+		}
+
+		function minus() {
+			const value = (
+				parseInt(prevNum.value) - parseInt(currentNum.value)
+			).toString();
+			currentNum.value = value;
+		}
+		function clear() {
+			currentNum.value = "";
+		}
+
+		const handleKeydown = (e: any) => pressed(e.key);
+
+		onMounted(() => {
+			window.addEventListener("keydown", handleKeydown);
+		});
+
+		onUnmounted(() => {
+			window.removeEventListener("keydown", handleKeydown);
+		});
 		return {
-			calculations,
+			currentNum,
 			pressed,
+			selectedOp,
+			prevNum,
 		};
 	},
 });
